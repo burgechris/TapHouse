809e69dc797ddc8933a53f05c3e8727566173982
Chris Burge <topher.burge@gmail.com>
2019-11-15 09:58:08 -0800

    add navbar component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import TapList from './TapList';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
+      <NavBar/>
       <div>
         <Switch>
-          {/* <Route exact path='/' component={HomePage} />
-          <Route exact path='/profile' component={ProfilePage} /> */}
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/tap' component={TapList}/>
         </Switch>
       </div>
     </div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <Link className="brand-logo" to='/'>TapHouse</Link>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to='/tap'>Tap List</Link></li>
+          <li><Link to='/new'>Add New</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
