e51e8a4dda23aa68e86f2d87dba04c71696e91e1
Chris Burge <topher.burge@gmail.com>
2019-11-15 10:10:50 -0800

    add taplist map function and first tap.

---
+++ src/components/TapList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Tap from './Tap';
+
+var tapList = [
+  {
+    name: 'Stout',
+    brand: 'Backwoods Brewing',
+    price: '$6',
+    abv: '7.2%'
+  }
+];
+
+function TapList() {
+  return (
+    <div className="container">
+      <div className="row">
+        {tapList.map((tap, index) => (
+          <Tap 
+            name={tap.name} 
+            brand={tap.brand} 
+            price={tap.price} 
+            abv={tap.abv}
+            key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TapList;
