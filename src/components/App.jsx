f469dd6ee8f0f3618e9294f1eae91d4b3b6bc740
Chris Burge <topher.burge@gmail.com>
2019-11-15 09:17:38 -0800

    install npm and plan out component structure.

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <Switch>
+          {/* <Route exact path='/' component={HomePage} />
+          <Route exact path='/profile' component={ProfilePage} /> */}
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
