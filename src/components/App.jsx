11fbf8db4c525f24f4803cd69fd57ce9537694a9
Chris Burge <topher.burge@gmail.com>
2019-11-17 19:47:11 -0800

    refactored home route and removed the HomeImg.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import NavBar from './NavBar';
 import Home from './Home';
 import TapList from './TapList';
+import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <HomeImg />
       <div className="row">
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exact path='/tap' component={TapList} />
+          <Route path='/tap' component={TapList} />
+          <Route component={Error404} />
         </Switch>
       </div>
     </div>
