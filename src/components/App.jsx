c4457b259a8184ac19a1843222e844899ee2c7ff
Chris Burge <topher.burge@gmail.com>
2019-11-22 08:06:11 -0800

    change the App component from functional to classical.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,19 +5,29 @@ import TapList from './TapList';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <div className="row">
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/tap' component={TapList} />
-          <Route component={Error404} />
-        </Switch>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainBeerList: {},
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="row">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/tap' component={TapList} />
+            <Route component={Error404} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
