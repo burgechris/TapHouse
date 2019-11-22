d266a3cf52ebabe6d1c62656156d9324d2296de6
Chris Burge <topher.burge@gmail.com>
2019-11-22 15:06:52 -0800

    add styling to tap list.

--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -1,46 +1,37 @@
 import React from 'react';
 import Tap from './Tap';
 import PropTypes from 'prop-types';
-// import Stout from '../assets/img/stout.jpg';
-
-// var mainTapList = [
-//   {
-//     img: Stout,
-//     name: 'Stout',
-//     brand: 'Backwoods Brewing',
-//     price: '$6',
-//     abv: '7.2%',
-//   },
-//   {
-//     img: Stout,
-//     name: 'Lager',
-//     brand: 'Backwoods Brewing',
-//     price: '$6',
-//     abv: '5.2%'
-//   },
-//   {
-//     img: Stout,
-//     name: 'IPA',
-//     brand: 'Backwoods Brewing',
-//     price: '$7',
-//     abv: '9.2%'
-//   }
-// ];
+import DarkBeer from '../assets/img/beer_dark.jpeg';
+import Bkgrnd from '../assets/img/wood_bkgrnd.jpeg'; 
 
 function TapList(props) {
+  // var tapListStyle = {
+  //   marginTop: '60px'
+  // };
+
+  var tapListBackground = {
+    backgroundImage: `url(${Bkgrnd})`,
+    backgroundAttachment: 'fixed',
+    height: '900px',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  };
+  
   return (
-    <div className="container">
+    <div style={tapListBackground}>
       <div className="row">
         {Object.keys(props.tapList).map(function (tapId) {
           var tap = props.tapList[tapId];
-          return <Tap 
-            img={tap.img}
+          return <Tap
+            img={DarkBeer}
             name={tap.name}
             brand={tap.brand}
             price={tap.price}
             abv={tap.abv}
             key={tapId}
-            tapId={tapId} />; })}
+            tapId={tapId} />;
+        })}
       </div>
     </div>
   );
