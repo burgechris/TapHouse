5db6d666433b31ac1fabb30381350eb1fd1238a9
Chris Burge <topher.burge@gmail.com>
2019-11-22 09:50:30 -0800

    refactor input form.

--- src/components/NewTapForm.jsx
+++ src/components/NewTapForm.jsx
@@ -10,57 +10,25 @@ function NewTapForm(props) {
 
   function handleNewTapFormSubmission(event) {
     event.preventDefault();
-    props.NewTapForm({})
+    props.onNewTap({name: _name.value, brand: _brand.value, price: _price.value, abv: _abv.value});
   }
 
   return (
     <div class="row">
-      <form class="col s12">
+      <form onSubmit={handleNewTapFormSubmission} class="col s12">
         <div class="row">
           <div class="input-field col s6">
-            <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-              <label for="first_name">First Name</label>
+            <input placeholder="Placeholder" id="name" type="text" class="validate"/>
+            <label for="name">Name</label>
+          </div>    
         </div>
-            <div class="input-field col s6">
-              <input id="last_name" type="text" class="validate">
-                <label for="last_name">Last Name</label>
-        </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-                  <label for="disabled">Disabled</label>
-        </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="password" type="password" class="validate">
-                    <label for="password">Password</label>
-        </div>
-                </div>
-                <div class="row">
-                  <div class="input-field col s12">
-                    <input id="email" type="email" class="validate">
-                      <label for="email">Email</label>
-        </div>
-                  </div>
-                  <div class="row">
-                    <div class="col s12">
-                      This is an inline input field:
-          <div class="input-field inline">
-                        <input id="email_inline" type="email" class="validate">
-                          <label for="email_inline">Email</label>
-                          <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-          </div>
-                      </div>
-                    </div>
-    </form>
-                </div>
-                )
-              }
+      </form>
+    </div>
+  );
+}
 
-NewTapForm.PropTypes = {
-                  onNewTap: PropTypes.func
-            };
+NewTapForm.propTypes = {
+  onNewTap: PropTypes.func
+};
             
 export default NewTapForm;
